ceb2a07f28a4455c28b8659dbba191f7834c5565
Mason Bassett <bassettmason@gmail.com>
2018-02-28 16:08:15 -0800

    test

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# selfie
+selfie.js
+=========
+
+An angular.js module that allows your users to create and save a selfie with their webcam via the getUserMedia() API
+
+Checkout http://schizoduckie.github.io/selfie.js/ for a demo.
+
+License: WTFPL 
---
+++ selfie.js
@@ -0,0 +1,20 @@
+function startup(){
+    var selfieCam = document.getElementById('selfie-cam');
+
+    navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+    
+    if (navigator.getUserMedia) {
+        navigator.getUserMedia({
+            video:true,
+            audio:false
+        }, function (stream) {
+            window.URL = window.URL || webkitURL;
+            var streamURL = window.URL.createObjectURL(stream);
+            selfieCam.src =streamURL;
+
+        }, function (error) {
+            console.warn(error);
+        });    
+    }   
+}
+window.addEventListener('load', startup);
